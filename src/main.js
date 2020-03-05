189c8a57a1d34775cb4d83c6687289e3f3eb6eac
Kenny Lindley <kenny@simplymadeapps.com>
2020-03-05 13:17:07 -0600

    Update action

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ const core = require("@actions/core");
 const fs = require("fs");
 
 function run() {
-  core.info(JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, "utf8")));
+  core.debug(fs.readFileSync(process.env.GITHUB_EVENT_PATH, "utf8"));
+  const releaseInfo = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, "utf8"));
 }
 
 run();
