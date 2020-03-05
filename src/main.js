de5a53eb41f86ff8fa07cc4bd78b8ff0a970482a
Kenny Lindley <kenny@simplymadeapps.com>
2020-03-05 10:58:54 -0600

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import core from "@actions/core";
+import { GitHub } from "@actions/github";
+
+async function run() {
+  const userName = core.getInput("user_name");
+  const repoName = core.getInput("repo_name");
+
+  try {
+    const github = new GitHub(process.env.GITHUB_TOKEN)
+    const releaseResponse = await github.repos.getLatestRelease(
+      userName,
+      repoName
+    );
+
+    core.debug(releaseResponse);
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run();
