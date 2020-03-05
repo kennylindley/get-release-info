1a45e91b986e6e34afbe8e966d77d4aa04b5b554
Kenny Lindley <kenny@simplymadeapps.com>
2020-03-05 14:49:26 -0600

    Build the dist

--- dist/index.js
+++ dist/index.js
@@ -53,6 +53,7 @@ function run() {
   core.debug(fs.readFileSync(process.env.GITHUB_EVENT_PATH, "utf8"));
   const releaseInfo = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, "utf8"));
 
+  console.log(releaseInfo);
   releaseInfo.assets.forEach(asset => {
     core.setOutput(asset.name, asset.browser_download_url);
   });
