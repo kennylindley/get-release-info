aa8aaf52e064fd907be626859f8d91924c81430a
Kenny Lindley <kenny@simplymadeapps.com>
2020-03-09 11:54:29 -0500

    Changes to the outputs

--- dist/index.js
+++ dist/index.js
@@ -54,14 +54,17 @@ const matchingRegex = new RegExp(core.getInput("matching_regex", { required: fal
 function run() {
   const releaseInfo = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, "utf8"));
   let count = 0;
+  let urls = "";
   releaseInfo.release.assets.forEach(asset => {
     // Only get the name if it is a .exe or .dmg
     if (matchingRegex && assets.name.match(matchingRegex)) return;
+    if (count > 0) {
+      urls.concat(",");
+    }
+    urls.concat(asset.browser_download_url);
     count++;
-    core.setOutput(`name[${count}]`, asset.name);
-    core.setOutput(`url[${count}]`, asset.url);
   });
-  core.setOutput("count", count)
+  core.setOutput("urls")
   core.setOutput("version", releaseInfo.release.name);
 }
 
