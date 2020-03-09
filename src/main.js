ef47e3cb45381a9d2016cc7153ebc46671b3a752
Kenny Lindley <kenny@simplymadeapps.com>
2020-03-09 12:39:57 -0500

    Debugging

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ const fs = require("fs");
 
 function run() {
   const releaseInfo = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, "utf8"));
-  console.log(releaseInfo.release.assets);
+
   let count = 0;
   let urls = "";
   releaseInfo.release.assets.forEach(asset => {
+    console.log(asset);
     if (count > 0) {
       urls.concat(",");
     }
