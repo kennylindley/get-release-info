c94ce7fef019ab966c144023ffad5d05cb690ac1
Kenny Lindley <kenny@simplymadeapps.com>
2020-03-09 12:19:22 -0500

    Updated removing the regex because it isn't working

--- dist/index.js
+++ dist/index.js
@@ -49,15 +49,11 @@ module.exports =
 const core = __webpack_require__(470);
 const fs = __webpack_require__(747);
 
-const matchingRegex = new RegExp(core.getInput("matching_regex", { required: false }));
-
 function run() {
   const releaseInfo = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, "utf8"));
   let count = 0;
   let urls = "";
   releaseInfo.release.assets.forEach(asset => {
-    // Only get the name if it is a .exe or .dmg
-    if (matchingRegex && asset.name.match(matchingRegex)) return;
     if (count > 0) {
       urls.concat(",");
     }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
 const core = require("@actions/core");
 const fs = require("fs");
 
-const matchingRegex = new RegExp(core.getInput("matching_regex", { required: false }));
-
 function run() {
   const releaseInfo = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, "utf8"));
   let count = 0;
   let urls = "";
   releaseInfo.release.assets.forEach(asset => {
-    // Only get the name if it is a .exe or .dmg
-    if (matchingRegex && asset.name.match(matchingRegex)) return;
     if (count > 0) {
       urls.concat(",");
     }
