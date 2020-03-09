79649d2f329c395974bd436eb85458dba4efe750
Kenny Lindley <kenny@simplymadeapps.com>
2020-03-09 08:42:47 -0500

    Modified to be able to include a regex for the filename

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 const core = require("@actions/core");
 const fs = require("fs");
 
+const matchingRegex = new RegExp(core.getInput("matching_regex", { required: false }));
+
 function run() {
   const releaseInfo = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, "utf8"));
 
   releaseInfo.release.assets.forEach(asset => {
+    // Only get the name if it is a .exe or .dmg
+    if (matchingRegex && assets.name.match(matchingRegex)) return;
     core.setOutput("name", asset.name);
     core.setOutput("url", asset.url);
   });
