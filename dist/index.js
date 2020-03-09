69c8d4aba50dccd4ba78cb9a8d02b47ab3c41f94
Kenny Lindley <kenny@simplymadeapps.com>
2020-03-09 09:19:10 -0500

    Created the dist

--- dist/index.js
+++ dist/index.js
@@ -49,13 +49,19 @@ module.exports =
 const core = __webpack_require__(470);
 const fs = __webpack_require__(747);
 
+const matchingRegex = new RegExp(core.getInput("matching_regex", { required: false }));
+
 function run() {
   const releaseInfo = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, "utf8"));
-
+  let count = 0;
   releaseInfo.release.assets.forEach(asset => {
-    core.setOutput("name", asset.name);
-    core.setOutput("url", asset.url);
+    // Only get the name if it is a .exe or .dmg
+    if (matchingRegex && assets.name.match(matchingRegex)) return;
+    count++;
+    core.setOutput(`name[${count}]`, asset.name);
+    core.setOutput(`url[${count}]`, asset.url);
   });
+  core.setOutput("count", count)
   core.setOutput("version", releaseInfo.release.name);
 }
 
